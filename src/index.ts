56948785511d665a44e0640f1ea9fe2231b16a14
Amaechi Precious <preciousamaechi111@yahoo.com>
2025-02-06 10:30:34 +0100

    debug

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to the Number Classification API" });
 });
 
